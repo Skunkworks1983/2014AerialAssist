7455932dd67741c03273a41919e43f4a3a43cfa4
Trust Issues <skunk.trustissues@gmail.com>
2014-02-24 18:46:40 -0800

    Made last commit actually build
    Autonomous.cpp was missing. Whoops

---
+++ Plutonium2014/Commands/Autonomous/Autonomous.cpp
@@ -0,0 +1,102 @@
+#include "Autonomous.h" 
+#include "../Automatic/AutoDriveDistance.h"
+//#include "../Automatic/TurnDegree.h" //100% legit not from last year guise
+//#include "../Automatic/CollectorShooterLoad.h"
+//#include "../Collector/Collect.h"
+//#include "../Collector/EjectDisks.h"
+//#include "../Collector/MoveCollectorArm.h"
+//#include "../Automatic/DiddlerDrive.h"
+//
+//#include "../CommandStarter.h"
+//#include "../CommandCanceler.h"
+//#include "../Shooter/Shoot.h"
+//#include "../Shooter/ChangeShooterPitch.h"
+//#include "../Shooter/ArmShooter.h"
+//#include "../Drivebase/Shift.h"
+//#include "../WaitForTrigger.h"
+//#include "../WaitForCommand.h"
+
+#define AUTO_SCRIPT_CHARMASK(a,b) ((((int)a) << 8) | ((int)b))
+
+Autonomous::Autonomous() :
+	CommandGroup("Autonomous-Blank") {
+}
+
+Autonomous::Autonomous(char *style) :
+	CommandGroup(style) {
+}
+
+Autonomous::Autonomous(int argc, char **argv) :
+	CommandGroup("Autonomous") {
+	int i = 0;
+	char typeA = '0', typeB = '0', cmdType = '0';
+	float arg = 0.0;
+	for (i = 0; i < argc; i++) {
+		if (argv[i][0] == '#') {
+			continue;
+		}
+		sscanf(argv[i], "%c\t%c%c\t%f", &cmdType, &typeA, &typeB, &arg);
+		Command *use = NULL;
+		switch (AUTO_SCRIPT_CHARMASK(typeA,typeB)) {
+		case AUTO_SCRIPT_CHARMASK('d','d'):
+			use = new AutoDriveDistance(arg);
+			break;
+//		case AUTO_SCRIPT_CHARMASK('t','d'): 
+//			use = new TurnDegree(arg);
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('d','s'):
+//			use = new Shift((Shift::ShiftType) ((int) arg));
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('w','f'):
+//			use = new WaitCommand(arg);
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('w','u'):
+//			use = new WaitUntilCommand(arg);
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('c','n'):
+//			use = new Collect(arg);
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('c','p'):
+//			use = new MoveCollectorArm(arg);
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('a','s'):
+//			use = new ArmShooter((ArmShooter::ArmType) ((int) arg));
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('s','s'):
+//			use = new Shoot();
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('s','p'):
+//			use = new ChangeShooterPitch(arg);
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('c','a'):
+//			use = new CollectorShooterLoad();
+//			break;
+//		case AUTO_SCRIPT_CHARMASK('c','e'):
+//			use = new EjectDisks((Collector::MotorDirection) ((int) arg));
+//			break;
+		default:
+			break;
+		}
+		if (use != NULL) {
+			switch (cmdType) {
+			case 's':
+				AddSequential(use);
+				break;
+			case 'p':
+				AddParallel(use);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
+Autonomous::~Autonomous() {
+
+}
+
+void Autonomous::Initialize() {
+//	CommandBase::shooter->setControlScheme(SHOOTER_DEFAULT_CONTROL);
+//	CommandBase::shooter->setWaitScheme(SHOOTER_DEFAULT_WAIT);
+}
